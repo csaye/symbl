7acf10ec4e23833bfe5e25c83210784cf63b10ec
Cooper Saye <coopersaye@gmail.com>
2021-07-16 22:39:33 -0700

    Implement bytelist management.

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,8 @@
+# globals
 index = 0
-pointer = 0
 byte = 256
+bytelist = [0 for i in range(byte)]
+pointer = 0
 
 # increments pointer by one
 def increment_pointer():
@@ -14,10 +16,22 @@ def decrement_pointer():
     if pointer <= 0: pointer = byte - 1
     else pointer -= 1
 
+# increments byte at pointer by one
+def increment_byte():
+    if bytelist[pointer] >= byte - 1: bytelist[pointer] = 0
+    else bytelist[pointer] += 1
+
+# decrements byte at pointer by one
+def decrement_byte():
+    if bytelist[pointer] <= 0: bytelist[pointer] = byte - 1
+    else bytelist[pointer] -= 1
+
 # processes given char
 def process(char):
     if char == '>': increment_pointer()
-    else if char == '<': decrement_pointer()
+    elif char == '<': decrement_pointer()
+    elif char == '+': increment_byte()
+    elif char == '-': decrement_byte()
 
 # parses given program
 def parse(program):
